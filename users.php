961e9aaecaf65150455d002fb1bc028a658db031
ayushbgi <ayush@businessgateways.com>
2019-11-13 18:05:48 +0530

    ayush-3

--- users.php
+++ users.php
@@ -28,7 +28,7 @@ else{
 <ul>
   <li><a href="admin.php">Home</a></li>
   <li><a href="#news">Statistics</a></li>
-  <li><a href="#news"  class="active">Users</a></li>
+  <li><a href="users.php"  class="active">Users</a></li>
   <li class="dropdown" style="float:right;">
     <a href="javascript:void(0)" class="dropbtn">Admin</a>
     <div class="dropdown-content">
@@ -38,5 +38,45 @@ else{
     </div>
   </li>
 </ul>
+ <table class="table1">
+   <tr>
+    <th>Id</th>
+    <th>Name</th>
+    <th>Email</th>
+  </tr>
+<?php
+ $query="SELECT * FROM emp";
+   $result = mysqli_query($conn,$query);
+   if (!$result) echo "INSERT failed: $query<br>".$connection->error . "<br><br>";
+
+   else{ 
+    while($row=mysqli_fetch_row($result)){
+
+     ?>
+
+    
+  <tr>
+    <td><?php echo $row[0]; ?></td>
+    <td><?php echo $row[1]; ?></td>
+    <td><?php echo $row[3]; ?></td>
+  </tr>
+ 
+  
+
+
+<?php
+                   
+
+   }
+
+
+   }
+
+
+
+
+?>
+</table>
+
 </body>
 </html>
